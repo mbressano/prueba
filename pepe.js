420224ef9a602e3ac761c41b22e830e101ccfa1a
bressano <mbressano@hotmail.com>
2023-05-04 19:35:48 -0300

    Pureba

---
+++ pepe.js
@@ -0,0 +1,11 @@
+let a = 15;
+let div = 1
+while (div <= a) {
+  if (a % div == 0) {
+    console.log("el divisor es " + div);
+  }
+  div++;
+  //subidadfgdfgfd sdfsdfs dfsjksfskjfsf sdf sfsnkfs 
+  sefsfsfsfs
+  dfsdffsf
+}
